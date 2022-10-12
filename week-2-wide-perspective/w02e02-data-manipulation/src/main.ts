85ccccaf8cc7635581c519d4f6ac87204f6e5a40
Marcin Czaniecki <marcinczaniecki97@gmail.com>
2022-10-12 16:57:53 +0200

    Project preparation with vite

---
+++ week-2-wide-perspective/w02e02-data-manipulation/src/main.ts
@@ -0,0 +1,9 @@
+/* eslint-disable no-console */
+import { ordersFakeData } from './orders/orders.fake-data';
+
+import './orders/tiles/BestSaleTile';
+import './orders/tiles/TotalOrdersTile';
+import './orders/tiles/TotalIncomeTile';
+import './orders/tiles/AnnualOrdersTile';
+
+console.log(ordersFakeData);
